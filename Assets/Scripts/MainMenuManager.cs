80bcb458f4d8e352198301ad9c01aad91d2600cc
Pitchayaporn <pitchayaporn.j@kkumail.com>
2022-10-06 23:40:17 +0700

    Lab5Bonus

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
+
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource bgsoundfade;
+
     public void StartGame()
+    {
+        bgsoundfade.DOFade(0.0f, 1.0f);
+        Invoke("LoadScene", 1f);
+    }
+
+    private void LoadScene()
     {
         SceneManager.LoadScene(1);
     }
